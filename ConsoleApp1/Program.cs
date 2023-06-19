71d49c24877797fb19dfc29ddad0b291cc09dcd9
Shunty <84626239+Shunty17@users.noreply.github.com>
2023-06-19 15:55:02 +0300

    1: add variable

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,17 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello =)");
+            Console.WriteLine("Hello, World!");
+            //string aFriend= "Nikita";
+            string aFriend = "Nika";
+            //Console.WriteLine(aFriend);
+            Console.WriteLine("Hello" + " " + aFriend);
+            Console.WriteLine($"Hello {aFriend}");
+            string firstFriend = "Bob";
+            string secondFriend = "Karl";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} leters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} leters.");
         }
     }
 }
